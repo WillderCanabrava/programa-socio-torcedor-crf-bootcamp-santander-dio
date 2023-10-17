b1b7cae7c6407d1c0b6645d0fea554516f3b57f2
Willder Canabrava <willcanabrava@gmail.com>
2023-10-17 12:25:50 -0300

    Initial commit

---
+++ src/main/java/com/flamengo/programasociotorcedorcrf/ProgramaSocioTorcedorCrfApplication.java
@@ -0,0 +1,13 @@
+package com.flamengo.programasociotorcedorcrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaSocioTorcedorCrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaSocioTorcedorCrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flamengo/programasociotorcedorcrf/ProgramaSocioTorcedorCrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flamengo.programasociotorcedorcrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaSocioTorcedorCrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
